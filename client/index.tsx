0ff3b4f9f77ffd9bb131383448ca310cdf2ec90e
Bonnie Wang <bonnie.wang.423@gmail.com>
2023-11-15 15:58:59 +1300

    complete

--- client/index.tsx
+++ client/index.tsx
@@ -17,10 +17,10 @@ root.render(
    * TODO: replace the empty strings below with your own domain, clientId, and audience
    */
   <Auth0Provider
-    domain=""
-    clientId=""
+    domain="dev-xri5nhxcy03cgakm.au.auth0.com"
+    clientId="wS5oMWeY6bYRgeBJxmYr3SJtSN78tymD"
     redirectUri={window.location.origin}
-    audience=""
+    audience="https://fruits/api"
   >
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
